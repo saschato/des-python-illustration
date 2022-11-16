8236328b957db7168f7293ee9c79f79dc483901f
saschato <114434040+saschato@users.noreply.github.com>
2022-11-16 09:52:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,27 @@ from core.initial_permutation import IP
 from core.f.expansion_box import EBox
 from core.f.substitution_boxes import SBoxes
 from core.f.permutation_box import PBox
+from core.key_schedule import SubKeys
+from core.final_permutation import FP
 
 from copy import deepcopy
 
 class DES:
-    def __init__(self):
+    def __init__(self,key):
+        # loading required classes
         self.IP = IP()
         self.EBox = EBox()
         self.SBoxes = SBoxes()
         self.PBox = PBox()
+        self.SubKeys = SubKeys(key)
+        self.FP = FP()
 
     def length(self,inp,n):
-        if not len(inp) == n:
+        if len(inp) <= n:
             while len(inp) < n:
                 inp = "0" + inp
+        else:
+            raise Exception
         return inp
 
     def f_function(self,right_site,key):
@@ -37,9 +44,9 @@ class DES:
     def split_plaintext(self,plaintext):
         return plaintext[0:32],plaintext[32:64]
 
-    def main(self,inp,key):
+    def main(self,inp):
         # raising exception if length or key or plaintext != 64
-        if not len(inp) == 64 or not len(key) == 48: # 56
+        if not len(inp) == 64:
             raise Exception
 
         # initial permutation
@@ -48,8 +55,8 @@ class DES:
         # splitting plaintext into two 32bit parts
         left_site,right_site = self.split_plaintext(inp)
 
-        for i in range(0,1):
-            temp_right_site = self.f_function(right_site,key)
+        for i in range(0,16):
+            temp_right_site = self.f_function(right_site,self.SubKeys.generate())
 
             # xor of current leftside and rightside after f function
             temp_left_site = self.length(bin(int(left_site,2)^int(temp_right_site,2))[2:],32)
@@ -58,8 +65,8 @@ class DES:
             left_site = deepcopy(right_site)
             right_site = deepcopy(temp_left_site)
 
-        print(left_site,right_site)
+        print(hex(int(self.FP.permute(right_site+left_site),2)))
 
 if __name__ == "__main__":
-    DES = DES()
-    DES.main("0000000000000000000000000000000000100000000000000000000000000000","111111111111111111111111111111111111111111111111")
+    DES = DES("0111000001100001011100110111001101110111011011110111001001100100")
+    DES.main("0"+bin(int(b"password".hex(),16))[2:])
